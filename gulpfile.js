e0958884b6d67f5d29230ed34ee6363497d12c63
ihor <evroigor86@gmail.com>
2023-06-19 16:38:20 +0300

    change back

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@ function watching() {
 }
 
 function cleanDist() {
-  return src('public', { allowEmpty: true, read: false }).pipe(clean());
+  return src('dist', { allowEmpty: true, read: false }).pipe(clean());
 }
 
 function building() {
@@ -131,7 +131,7 @@ function building() {
     {
       base: 'app',
     },
-  ).pipe(dest('public'));
+  ).pipe(dest('dist'));
 }
 
 exports.styles = styles;

502b6240142446aa92a5f9cb0ff60a329b327c36
ihor <evroigor86@gmail.com>
2023-06-20 19:24:45 +0300

    refactored form

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,35 +1,26 @@
 // form validation starts
-$.validator.addMethod(
-  'validName',
-  function (value, element) {
-    return this.optional(element) || /^[a-zA-Zа-яА-ЯёЁіІїЇєЄ]{3,20}$/.test(value);
-  },
-  'Please enter a valid name (3-20 characters, letters only).',
-);
+$.validator.addMethod('validName', function (value, element) {
+  return this.optional(element) || /^[a-zA-Zа-яА-ЯёЁіІїЇєЄ]{3,20}$/.test(value);
+});
 
-$.validator.addMethod(
-  'validPhoneNumber',
-  function (value, element) {
-    return this.optional(element) || /^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/.test(value);
-  },
-  'Please enter a valid phone number (+38 (999) 99-99-999).',
-);
+$.validator.addMethod('validPhoneNumber', function (value, element) {
+  return this.optional(element) || /^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/.test(value);
+});
 
-$.validator.addMethod(
-  'validEmail',
-  function (value, element) {
-    return (
-      this.optional(element) ||
-      /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-яЁёІіЇїЄє]{1}[-0-9А-яЁёІіЇїЄє\.]{1,}[0-9А-Яа-яЁёІіЇїЄє]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u.test(
-        value,
-      )
-    );
-  },
-  'Please enter a valid email address.',
-);
+$.validator.addMethod('validEmail', function (value, element) {
+  return (
+    this.optional(element) ||
+    /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-яЁёІіЇїЄє]{1}[-0-9А-яЁёІіЇїЄє\.]{1,}[0-9А-Яа-яЁёІіЇїЄє]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u.test(
+      value,
+    )
+  );
+});
 
 $(document).ready(function () {
-  $('#myWebinarForm').validate({
+  const webinarForm = $('#myWebinarForm');
+  const submitButton = webinarForm.find('.form-button');
+
+  webinarForm.validate({
     rules: {
       name: {
         required: true,
@@ -42,67 +33,156 @@ $(document).ready(function () {
       email: {
         required: true,
         validEmail: true,
+        email: true, // Добавлено правило email
       },
     },
     messages: {
       name: {
-        required: 'Пожалуйста, введите ваше имя',
-        validName: 'Пожалуйста введите корректное имя',
+        required: 'Поле не может быть пустым',
+        validName: 'Введите корректное имя',
       },
       phone: {
-        required: 'Пожалуйста, введите ваш номер',
-        validPhoneNumber: 'Пожалуйста введите корректный номер',
+        required: 'Поле не может быть пустым',
+        validPhoneNumber: 'Введите корректный номер',
       },
       email: {
-        required: 'Пожалуйста, введите ваш email',
-        validEmail: 'Пожалуйста введите корректный email',
+        required: 'Поле не может быть пустым',
+        validEmail: 'Введите корректный email',
+        email: '', // Установлено пустое сообщение для правила email
       },
     },
-    ignore: '.country-code-select',
     onkeyup: function (element) {
       $(element).valid();
     },
-
     errorPlacement: function (error, element) {
-      error.addClass('error-message');
-      error.appendTo(element.parent());
+      const formControl = $(element).closest('.form__control');
+      formControl.addClass('error');
+      formControl.find('.error-message').text(error.text());
+    },
+    highlight: function (element) {
+      const formControl = $(element).closest('.form__control');
+      formControl.addClass('error').removeClass('success');
+      formControl.find('.error-message').text('');
+      submitButton.prop('disabled', true);
+    },
+    unhighlight: function (element) {
+      const formControl = $(element).closest('.form__control');
+      formControl.removeClass('error').addClass('success');
+      formControl.find('.error-message').text('');
+
+      if (webinarForm.find('.error').length === 0) {
+        submitButton.prop('disabled', false);
+      }
     },
   });
+  // form validation ends
+
+  // form submit starts
+  // const form = webinarForm[0]; // Получаем DOM-элемент формы
+
+  // form.addEventListener('submit', formSubmit);
+
+  // async function formSubmit(e) {
+  //   e.preventDefault();
+
+  //   // Проверяем валидность формы
+  //   if (webinarForm.valid()) {
+  //     const data = serializeForm(form);
+  //     const response = await sendData(data);
+
+  //     if (response.ok) {
+  //       let result = await response.json();
+  //       alert(result.message);
+  //       form.reset();
+  //     } else {
+  //       alert('Код ошибки: ' + response.status);
+  //     }
+  //   }
+  // }
+
+  // function serializeForm(formNode) {
+  //   return new FormData(form);
+  // }
+
+  // function sendData(data) {
+  //   return fetch('../send_mail.php', {
+  //     method: 'POST',
+  //     body: data,
+  //   });
+  // }
+  async function formSubmit(e) {
+    e.preventDefault();
+
+    try {
+      if (webinarForm.valid()) {
+        const data = serializeForm(form);
+        const response = await sendData(data);
+
+        if (response.ok) {
+          let result = await response.json();
+          alert(result.message);
+          form.reset();
+        } else {
+          alert('Код ошибки: ' + response.status);
+        }
+      }
+    } catch (error) {
+      console.error('Произошла ошибка:', error);
+    }
+  }
+
+  const form = webinarForm[0]; // Получаем DOM-элемент формы
+  form.addEventListener('submit', formSubmit);
+
+  function serializeForm(formNode) {
+    return new FormData(form);
+  }
+
+  async function sendData(data) {
+    try {
+      return await fetch('../send_mail.php', {
+        method: 'POST',
+        body: data,
+      });
+    } catch (error) {
+      console.error('Произошла ошибка при отправке данных:', error);
+      throw error;
+    }
+  }
 });
 
 let phoneInput = document.querySelector('input[type="tel"]');
 let im = new Inputmask('+38 (999) 999-99-99');
 im.mask(phoneInput);
-// form validation ends
 
 // form submit starts
-const form = document.forms['myWebinarForm'];
+// const form = document.forms['myWebinarForm'];
 
-form.addEventListener('submit', formSubmit);
+// form.addEventListener('submit', formSubmit);
 
-async function formSubmit(e) {
-  e.preventDefault();
-  const data = serializeForm(form);
-  const response = await sendData(data);
-  if (response.ok) {
-    let result = await response.json();
-    alert(result.message);
-    form.reset();
-  } else {
-    alert('Код ошибки: ' + response.status);
-  }
-}
+// async function formSubmit(e) {
+//   e.preventDefault();
+//   const data = serializeForm(form);
+//   const response = await sendData(data);
+//   if (response.ok) {
+//     let result = await response.json();
+//     alert(result.message);
+//     form.reset();
+//   } else {
+//     alert('Код ошибки: ' + response.status);
+//   }
+// }
 
-function serializeForm(formNode) {
-  return new FormData(form);
-}
+// function serializeForm(formNode) {
+//   return new FormData(form);
+// }
 
-async function sendData(data) {
-  return await fetch('../send_mail.php', {
-    method: 'POST',
-    body: data,
-  });
-}
+// async function sendData(data) {
+//   return await fetch('../send_mail.php', {
+//     method: 'POST',
+//     body: data,
+//   });
+// }
 // form submit ends
 
 // gsap animation starts
